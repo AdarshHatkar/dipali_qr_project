6af9c079304181adada410a06fc4f737393e97bc
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-08-11 16:01:53 +0530

    removed empty lines

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
 let qrDiv =document.getElementById("qrcode")
 let qrcodePrent =document.getElementById("qrcodePrent")
-
 function generateQr(link){
 	new QRCode(qrDiv,  {
 		text: link,
@@ -10,19 +8,13 @@ function generateQr(link){
 		colorDark : "#000000",
 		colorLight : "#ffffff",
 		correctLevel : QRCode.CorrectLevel.H,
-		
 	});
 }
-
 let linkInput = document.getElementById('linkInput')
 let headingInput = document.getElementById('headingInput')
-
 let qrHeading = document.getElementById('qrHeading')
-
 let qrButton = document.getElementById('qrButton')
-
 let printBtn = document.getElementById('printBtn')
-
 qrButton.addEventListener('click',()=>{
 	let linkValue= linkInput.value
 	let headingValue=headingInput.value
@@ -33,31 +25,24 @@ qrButton.addEventListener('click',()=>{
 		console.log(error)
 		window.alert("Invalid url , please enter valid url ")
 	}
-	
 	if(isUrlCorrect){
 		// console.log(isUrlCorrect)
 	generateQr(linkValue)
 	const h1 = document.createElement("h1");
 const textNode = document.createTextNode(headingValue);
 h1.appendChild(textNode);
-
 	qrHeading.appendChild(h1)
 	printBtn.classList.remove("d-none");
 	}
-
 })
-
 printBtn.addEventListener("click",()=>{
 	var divContents = qrcodePrent.innerHTML;
 	console.log(divContents)
 	var a = window.open('', '', 'height=500, width=500');
 	a.document.write('<html>');
-
 	a.document.write('<body >');
 	a.document.write(divContents);
 	a.document.write('</body></html>');
 	a.document.close();
 	a.print();
 })
-
-
